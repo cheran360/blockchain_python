d57f0c248d622f27c14f284f56af177e64691fe5
cheran <cherankumar.548@gmail.com>
2022-03-26 20:53:07 +0530

    new test case add(use of pytest)

--- brownie_fund_me/tests/test_fund_me.py
+++ brownie_fund_me/tests/test_fund_me.py
@@ -1,5 +1,8 @@
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from scripts.deploy import deploy_fund_me
+from brownie import network, accounts, exceptions
+import pytest
+
 
 def test_can_fund_and_withdraw():
     account = get_account()
@@ -11,3 +14,15 @@ def test_can_fund_and_withdraw():
     tx2 = fund_me.withdraw({"from":account})
     tx2.wait(1)
     assert fund_me.addressToAmountFunded(account.address) == 0
+
+# for skipping the test for rinkeby flag(in cmd)(see video at 5.52.16)
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip('only for local testing')
+    
+    fund_me = deploy_fund_me()
+    # gives a blank random account for testing purposes..
+    bad_actor = accounts.add()
+    # if fund_me.withdraw({"from":bad_actor}) reverts,(we want it to revert) test case passed
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from":bad_actor})
